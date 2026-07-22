5cd0d2be5a8942226ebc07f3e9f92fffad404156
Thomas Mithamo <104732569+Davinci-Developer-Official@users.noreply.github.com>
2026-07-22 14:30:12 +0300

    Delete webScrapping directory

--- webScrapping/fetch_page.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-def fetch_page(url):
-    resp = requests.get(url)
-    resp.raise_for_status()
-    return resp.text
--- webScrapping/parse_page.py
+++
@@ -1,7 +0,0 @@
-from bs4 import BeautifulSoup
-
-def parse_page(html):
-    soup = BeautifulSoup(html,"html.parser")
-    title = soup.find("h1").get_text(strip=True)
-    paragraphs = [p.get_text("",strip=True) for p in soup.find_all("p")]
-    return {"title":title,"paragraphs":paragraphs}
